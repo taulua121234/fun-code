e4225ce5f3cf48120bfb3593f8aaddc52789f9e5
taulua121234 <145206954+taulua121234@users.noreply.github.com>
2023-10-17 20:48:09 +0700

    Update công cụ tính điểm.py

--- Tự học python/công cụ tính điểm.py
+++ Tự học python/công cụ tính điểm.py
@@ -10,8 +10,9 @@ if S <= 4 :
     print('Điểm thi mới: ',Snew)
 elif 4<= S <= 5.4:
     print('Điểm tổng kết: ',S)
+    print('Bạn có thể học cải thiện điểm')
     ykien = bool(input('Bạn đã học cải thiện điểm chưa: (True/False) '))
-    if ykien := True :
+    if ykien == True :
         Tnew = float(input('Điểm thi mới: '))
         Qnew = float(input('Điểm quá trình mới: '))
         Snew = max(k*Tnew + (1-k)*Qnew, 1/2*(k*Tnew + (1-k)*Qnew + S),4)
